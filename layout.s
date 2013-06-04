525c29fd0c267e31152751d87cf688552d152dc2
Tony Fruzza <tony@lightspeedsystems.com>
2013-06-04 16:49:39 -0700

    Added a couple zombies to the page layout for the demo.

--- layout.s
+++ layout.s
@@ -242,6 +242,256 @@ printSinglePlayerNextPieceBox
     jsr NewColors ; need to run this twice the first time
     rts
 
+printQuickZombieInBox
+    lda #10 ; width
+    pha
+    lda #5 ; height
+    pha
+
+    lda #$0A ; 1,546
+    pha
+    lda #$06
+    pha
+    jsr DrawBorderBox
+
+; The right side box
+    lda #10 ; Width
+    pha
+    lda #5 ; Height
+    pha
+    lda #$23
+    pha
+    lda #$06
+    pha
+    jsr DrawBorderBox
+
+pqzib_drawSomething
+    lda DrawRightSide
+    beq pqzib_DrawLeftSide
+    cmp #1
+    beq pqzib_drawRightSide
+    jmp pqzib_done
+    ; Draw Left side
+pqzib_DrawLeftSide
+    lda #$33 ; 1,587
+    sta zpPtr2
+    lda #$06
+    sta zpPtr2+1
+    jmp pqzib_draw
+
+pqzib_drawRightSide
+    ; Right Row 1/5
+    lda #$4C ; 1,587
+    sta zpPtr2
+    lda #$06
+    sta zpPtr2+1
+    jmp pqzib_draw
+
+pqzib_draw
+    ; Row 1/5
+    ldy #0
+    ldx #128
+    txa
+    sta (zpPtr2), y
+    inx
+    txa
+    iny
+    sta (zpPtr2), y
+    inx
+    txa
+    iny
+    sta (zpPtr2), y
+    iny
+    lda #' '
+    sta (zpPtr2), y
+    lda #143
+    iny
+    sta (zpPtr2), y
+    iny
+    lda #' '
+    sta (zpPtr2), y
+    ; Right Zombie
+    dex
+    dex
+    txa
+    iny
+    sta (zpPtr2), y
+    inx
+    txa
+    iny
+    sta (zpPtr2), y
+    inx
+    txa
+    iny
+    sta (zpPtr2), y
+
+
+    ; Row 2/5
+    ldy #40
+    inx
+    txa
+    sta (zpPtr2), y
+    inx
+    txa
+    iny
+    sta (zpPtr2), y
+    inx
+    txa
+    iny
+    sta (zpPtr2), y
+    iny
+    lda #' '
+    sta (zpPtr2), y
+    lda #144
+    iny
+    sta (zpPtr2), y
+    iny
+    lda #' '
+    sta (zpPtr2), y
+    ; Right Zombie
+    dex
+    dex
+    txa
+    iny
+    sta (zpPtr2), y
+    inx
+    txa
+    iny
+    sta (zpPtr2), y
+    inx
+    txa
+    iny
+    sta (zpPtr2), y
+
+
+
+    ; Row 3/5
+    ldy #80
+    inx
+    txa
+    sta (zpPtr2), y
+    inx
+    txa
+    iny
+    sta (zpPtr2), y
+    inx
+    txa
+    iny
+    sta (zpPtr2), y
+    iny
+    lda #' '
+    sta (zpPtr2), y
+    lda #145
+    iny
+    sta (zpPtr2), y
+    iny
+    lda #' '
+    sta (zpPtr2), y
+    ; Right Zombie
+    dex
+    dex
+    txa
+    iny
+    sta (zpPtr2), y
+    inx
+    txa
+    iny
+    sta (zpPtr2), y
+    inx
+    txa
+    iny
+    sta (zpPtr2), y
+
+
+
+    ; Row 4/5
+    ldy #120
+    inx
+    txa
+    sta (zpPtr2), y
+    inx
+    txa
+    iny
+    sta (zpPtr2), y
+    inx
+    txa
+    iny
+    sta (zpPtr2), y
+    iny
+    lda #' '
+    sta (zpPtr2), y
+    lda #146
+    iny
+    sta (zpPtr2), y
+    iny
+    lda #' '
+    sta (zpPtr2), y
+    ; Right Zombie
+    dex
+    dex
+    txa
+    iny
+    sta (zpPtr2), y
+    inx
+    txa
+    iny
+    sta (zpPtr2), y
+    inx
+    txa
+    iny
+    sta (zpPtr2), y
+
+
+
+    ; Row 5/5
+    ldy #160
+    inx
+    txa
+    sta (zpPtr2), y
+    inx
+    txa
+    iny
+    sta (zpPtr2), y
+    inx
+    txa
+    iny
+    sta (zpPtr2), y
+    iny
+    lda #' '
+    sta (zpPtr2), y
+    lda #147
+    iny
+    sta (zpPtr2), y
+    iny
+    lda #' '
+    sta (zpPtr2), y
+    ; Right Zombie
+    dex
+    dex
+    txa
+    iny
+    sta (zpPtr2), y
+    inx
+    txa
+    iny
+    sta (zpPtr2), y
+    inx
+    txa
+    iny
+    sta (zpPtr2), y
+
+    inc DrawRightSide
+    jmp pqzib_drawSomething
+pqzib_done
+    lda #0
+    sta DrawRightSide
+    jsr zombieColorSwap ; init color
+    rts
+DrawRightSide   .byte $00
+
+
+
+
 TOP_CLEAR1 .equ 1161
 TOP_CLEAR2 .equ 1162
 TOP_CLEAR3 .equ 1163
